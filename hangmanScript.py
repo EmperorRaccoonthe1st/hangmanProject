b8932783a4ec508fdcf7ec1072003c5a0ec5f717
emperorraccoonthe1st <owenmlopez07@gmail.com>
2023-04-12 09:54:05 -0400

    modified:   hangmanScript.py

--- hangmanScript.py
+++ hangmanScript.py
@@ -3,9 +3,14 @@ from tkinter import ttk
 root = Tk()
 
 mainFrame = ttk.Frame(root, padding="3i, 3i")
+baseWord = ""
+usedLetters = []
+display = {}
 
+def setDisplay():
+    pass
 
-
+baseWord = input("Input the word to be guessed: ")
 
 
 
