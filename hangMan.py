930ef5a521d8696bdce9a5bdd820aad1940a0a36
emperorraccoonthe1st <owenmlopez07@gmail.com>
2023-04-18 10:04:31 -0400

    modified:   hangMan.py

--- hangMan.py
+++ hangMan.py
@@ -2,7 +2,6 @@ from tkinter import *
 from tkinter import ttk
 #root = Tk()
 
-#Please work
 
 baseWord = ""
 usedLetters = []
@@ -27,9 +26,12 @@ def setDisplay(use, letter = ""):
 
 print("Welcome to Hangman")
 while gameState == True:
+    print("first Loop")
+    points = 0
     baseWord = input("Input the word to be guessed: ")
     wordGuessed = 5
     setDisplay("reset")
+    print(wordGuessed)
     while wordGuessed > 0:
         print(str(display))
         letterInput = str(input("Input a letter that is in the word: "))
@@ -40,12 +42,24 @@ while gameState == True:
                 print("Pasted")
                 print(letterInput + " was in the word")
                 setDisplay("edit", letterInput)
+                points += 1
+                if points == len(list(baseWord)):
+                    print("You Won!") 
+                    prompt = str(input("What do you want to do? Play Again or Quit: ")).upper()
+                    if prompt == "PLAY AGAIN":
+                        print("Lets play again!")
+                        break                       
+                    elif prompt == "QUIT":
+                        gameState = False
+                        break               
             else:
                 print(letterInput + " was not in the word")
                 wordGuessed -= 1
                 print("You have " + str(wordGuessed) + " many guesses left.")
     else:
         print("You ran out of guesses! Better luck next time")
+        break
+        
 
 
 
