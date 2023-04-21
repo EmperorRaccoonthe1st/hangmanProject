91e4e2f66a824d8afbef06dc8fb4ec4b86548b17
emperorraccoonthe1st <owenmlopez07@gmail.com>
2023-04-21 09:52:59 -0400

    modified:   hangMan.py

--- hangMan.py
+++ hangMan.py
@@ -16,22 +16,45 @@ def setDisplay(use, letter = ""):
                 display[x] = str(listWord[x])
 
 
+def setBaseWord(use):
+    global baseWord
+    if use == "set":
+        baseWord = list(input("Input a word to be guessed: "))
+        for x in range(len(baseWord)):
+            if isinstance(baseWord[x], str) == True:
+                #print(baseWord[x])
+                baseWord[x] = baseWord[x].upper()
+    #print(baseWord)
+
+
+def setLetter(use):
+    global letter
+    if use == "set":
+        letter = str(input("Guesse a letter in the word: "))
+        if isinstance(letter, str) == True:
+            letter = letter.upper()
+        
+
+
 def playRound():
-    global points, mistakes
+    global points, mistakes, letter
 
     if not points == len(list(baseWord)):
         print(display)
-        letter = str(input("Guesse a letter in the word: "))
+        print("You alread used these letters: ", usedLetters)
+        setLetter("set")
+        
         #print(usedLetters)
-        if not letter in usedLetters:
-            usedLetters.append(letter)
+        if not letter.upper() in usedLetters:
+            usedLetters.append(letter.upper())
             #print(letter, " is not in ", usedLetters)
-            if letter in baseWord:
+            if letter.upper() in baseWord:
                 points += 1
                 setDisplay("edit", letter)
                 print(letter, " was in the word!")
                 #print(display)
                 if points == len(list(baseWord)):
+                    print("The word was: ", display)
                     print("You Won!!!!")
             else:
                 mistakes -= 1
@@ -46,12 +69,15 @@ display = []
 mistakes = 5
 points = 0
 usedLetters = []
-baseWord = str(input("Put a word to be guessed: "))
+#baseWord = str(input("Put a word to be guessed: "))
+setBaseWord("set")
 setDisplay("reset")
 for x in range(25):
         print("#")
 while mistakes > 0 and not points == len(list(baseWord)):
     playRound()
+    print("#")
+    print("#")
 
 
 
